6bc8e631e961a7f86d6815ac5cafab6da173cf7e
koleq(NICOLEv2) <o.kurz@atlas.cz>
2021-11-18 09:00:24 +0100

    Add Muliplication & Division, Change to singles

--- main.pas
+++ main.pas
@@ -2,7 +2,7 @@ program HelloWorld;
 uses crt;
 
 var
-        a, b, c, cmd: Integer;
+        a, b, c, cmd: single;
 
 procedure PAKTC();
 begin
@@ -17,8 +17,8 @@ begin
         begin
                 writeln('Welcome to KolkulatorPAS (C)2021 koleq');
                 writeln();
-                writeln('1. Addition');
-                writeln('2. Subtraction');
+                writeln('1. Addition    4. Multiplication');
+                writeln('2. Subtraction 5. Division');
                 writeln('3. Exit');
                 writeln();
                 write('pick a choice: ');
@@ -32,7 +32,7 @@ begin
                         readln(b);
                         c := a + b;
                         ClrScr;
-                        writeln('Result of ', a, ' + ', b, ' = ', c);
+                        writeln('Result of ', a:0:2, ' + ', b:0:2, ' = ', c:0:2);
                         PAKTC;
                 end
                 else if cmd = 2 then
@@ -43,7 +43,29 @@ begin
                         readln(b);
                         c := a - b;
                         ClrScr;
-                        writeln('Result of ', a, ' - ', b, ' = ', c);
+                        writeln('Result of ', a:0:2, ' - ', b:0:2, ' = ', c:0:2);
+                        PAKTC;
+                end
+                else if cmd = 4 then
+                begin
+                        write('Please write your first number: ');
+                        readln(a);
+                        write('Please write your second number: ');
+                        readln(b);
+                        c := a * b;
+                        ClrScr;
+                        writeln('Result of ', a:0:2, ' * ', b:0:2, ' = ', c:0:2);
+                        PAKTC;
+                end
+                else if cmd = 5 then
+                begin
+                        write('Please write your first number: ');
+                        readln(a);
+                        write('Please write your second number: ');
+                        readln(b);
+                        c := a / b;
+                        ClrScr;
+                        writeln('Result of ', a:0:2, ' / ', b:0:2, ' = ', c:0:2);
                         PAKTC;
                 end
                 else
